74fe4e17d005727da101a5ea08cc56e093cd729f
FirasCh3 <firaschebil61@gmail.com>
2024-11-16 18:40:27 +0100

    started retrieving data and storing it in a file

--- Slave/src/Slave.rs
+++ Slave/src/Slave.rs
@@ -1,6 +1,7 @@
 use std::io::{Read, Write};
-use std::net::{Ipv4Addr, Shutdown, SocketAddrV4, TcpListener, TcpStream};
+use std::net::{Ipv4Addr, SocketAddrV4, TcpListener, TcpStream};
 use lib::Block;
+use std::fs::File;
 
 #[derive(Debug)]
 pub struct Slave {
@@ -9,10 +10,10 @@ pub struct Slave {
 }
 impl Slave {
     pub fn new(port: u16) -> Slave {
-        return Slave {
+        Slave {
             address: SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), port),
             port,
-        };
+        }
     }
     pub fn listen_and_serve(&self) -> std::io::Result<()> {
         let listener = TcpListener::bind(self.address)?;
@@ -21,16 +22,15 @@ impl Slave {
             match streams {
                 Ok(mut stream) => {
                     stream.read_to_string(&mut buffer)?;
-                    let deseralized: Block =  serde_json::from_str(buffer.as_str())?;
-                    println!("{:?}", deseralized.block_number())
-                    //println!("{:?}", serde_json::from_slice(&buffer)?);
-                    /*if (buffer[0] == 1) {
+                    let mut deserialized: Block =  serde_json::from_str(buffer.as_str())?;
+                    let content = deserialized.content();
+                    if (content[0] == 0){
+                        content.remove(0);
+                        let filename = String::new() + "../resources/" + deserialized.filename() + deserialized.block_number().to_string().as_str() + ".txt";
+                        let mut file = File::create(filename)?;
+                        println!("{:?}", String::from_utf8(deserialized.content().clone()));
+                    }
 
-                        println!("{:?}", std::str::from_utf8(&buffer).unwrap());
-                    } else {
-                        println!("{}", "writing");
-                        stream.write(b"response")?;
-                    }*/
                 }
                 Err(e) => {
                     println!("{:?}", e);
